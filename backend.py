8adcdb6e940a5117a5ca6d534aebfa3df2f9e76a
Jimi Jukkala <99808538+Jimpsoni@users.noreply.github.com>
2022-10-01 13:56:18 +0300

    Update backend.py
    Added event class

--- backend.py
+++ backend.py
@@ -1,1 +1,14 @@
+class Event:
+    def __init__(self, event_name, max_people):
+        # Parameters
+        self.name = event_name
+        self.max_people = max_people
 
+        # Other variables
+        self.people_attending = []
+
+    def join(self, person):
+        self.people_attending.append(person)
+
+    def cancel(self, person):
+        self.people_attending.remove(person)
